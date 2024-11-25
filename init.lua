87e21dcd326fa8c0907cbc727160a0a0ecb06bd9
benabub <fkhurkhd2123dfs@tutamail.com>
2024-11-25 12:26:25 +0500

    Big repo reorganization

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require 'core.config'
 require 'core.autocommands'
 require 'core.lazy'
 require 'core.keymappings'
+require 'core.health'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
