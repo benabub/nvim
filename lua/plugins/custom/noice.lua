b24c890525b8ca9107053eb4ec9cdab894cafa5e
benabub <fkhurkhd2123dfs@tutamail.com>
2024-12-09 19:14:07 +0500

    noice annoing popups off

--- lua/plugins/custom/noice.lua
+++ lua/plugins/custom/noice.lua
@@ -1,29 +1,43 @@
 return {
-  -- lazy.nvim
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
+{
+    "folke/noice.nvim",
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
-    },
-    config = function()
-      local noice = require 'noice'
-      noice.setup {
-        routes = {
-          {
-            view = 'cmdline',
-            filter = { event = 'msg_showmode' },
-          },
+      "MunifTanjim/nui.nvim",
+      {
+        "rcarriga/nvim-notify",
+        name = "notify",
+        opts = {
+          top_down = false,
+          stages = "static",
+          -- stylua: ignore
+          max_height = function() return math.floor(vim.o.lines * 0.75) end,
+          -- stylua: ignore
+          max_width = function() return math.floor(vim.o.columns * 0.75) end,
         },
-      }
-    end,
+      },
+    },
+    -- stylua: ignore
+    init = function() vim.opt.lazyredraw = false end,
+    opts = {
+      messages = { view = "mini", view_warn = "mini" },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+          ["vim.lsp.util.stylize_markdown"] = false,
+          ["cmp.entry.get_documentation"] = false,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+      routes = {
+        { filter = { event = "notify", find = "No information available" }, opts = { skip = true } },
+      },
+    },
   },
 }
