c0b4f56a9bb6db310f3e066f531a2bee5bdce07d
benabub <fkhurkhd2123dfs@tutamail.com>
2025-02-28 12:52:20 +0500

    toggleterm added

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -19,13 +19,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- Terminal
+vim.keymap.set('n', '<F5>', ':ToggleTerm size=40 dir=~/Desktop direction=float name=desktop<CR>') -- toggle
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -187,7 +182,7 @@ vim.keymap.set('n', '<leader>tw', function()
   require('neotest').watch.watch()
 end, { desc = 'watch current test for changes' })
 
--- Templates
+-- Templates for this file
 
 -- vim.keymap.set('n', '<leader>h', "<cmd>lua <cr>")
 
