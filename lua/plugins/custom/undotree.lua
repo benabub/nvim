d2b9fec9e2f8062a14551e4dd3eb5ffc1c723989
Benedict Abub <fkhurkhd2123dfs@tutamail.com>
2025-06-18 09:41:04 +0500

    lua/plugins/custom/undotree.lua: window resize

--- lua/plugins/custom/undotree.lua
+++ lua/plugins/custom/undotree.lua
@@ -1,9 +1,10 @@
-
 return {
-    "mbbill/undotree",
+  'mbbill/undotree',
 
-    config = function() 
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
+  config = function()
+    vim.g.undotree_WindowLayout = 2
+    vim.g.undotree_SplitWidth = 30
+    vim.g.undotree_DiffAutoOpen = 0
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+  end,
 }
-
