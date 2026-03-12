846a272533dc38573effae1a1db8788106185b95
Benedict Abub <fkhurkhd2123dfs@tutamail.com>
2026-03-12 14:48:50 +0500

    lua/plugin-specs/core/conform.lua: add dynamic timeout and ruff formatter for python

--- lua/plugin-specs/core/conform.lua
+++ lua/plugin-specs/core/conform.lua
@@ -13,13 +13,15 @@ return {
         desc = 'Format file without saving',
       },
     },
+
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
+        -- timeout increase
+        local line_count = vim.api.nvim_buf_line_count(bufnr)
+        local timeout = line_count > 10000 and 3000 or 500
+
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -27,17 +29,13 @@ return {
           lsp_format_opt = 'fallback'
         end
         return {
-          timeout_ms = 500,
+          timeout_ms = timeout,
           lsp_format = lsp_format_opt,
         }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        python = { 'ruff_format' }, -- ruff work both through LSP & conform
       },
     },
   },
